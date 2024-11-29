81cf698a0403f53fff121ed0273b94f398f72a14
Chiraphat-t <chiraphat.ttt@gmail.com>
2024-11-29 16:43:06 +0700

    done

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,22 @@
 using namespace std;
 
 int main(){
+    int n=1,x,y;
+
+    while (n!=0){
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >> n;
+   
+            if(n%2==0 && n!=0){
+            x=x+1;
+        }
+        if(n%2!=0){
+            y=y+1;
+        }
+    }
+    cout << "#Even numbers = " << x <<"\n";
+    cout << "#Odd numbers = "<<y;
+
+    
     return 0;
 }
